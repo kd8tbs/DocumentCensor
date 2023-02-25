107d0109ace6a8b8ce2b02d4050d33a8f9169de3
Gary Pinsky <kd8tbs@gmail.com>
2023-02-25 00:27:24 -0500

    uuuuuuuuu

--- Main.py
+++ Main.py
@@ -1,5 +1,8 @@
 import PyPDF2
+import re
+import tk
 from PyPDF2 import PdfReader
+from PyPDF2 import PageObject, PdfReader
 from FrontEnd import create_ui
 
 def extract_text_from_pdf(path):
@@ -13,55 +16,53 @@ def extract_text_from_pdf(path):
     return text
 
 
-def insert_text_into_pdf(path, text):
-    """
-    Inserts text into a PDF file and saves the updated file.
-    """
-    # Open the PDF file in read-binary mode.
-    with open(path, 'rb') as file:
-        # Create a PDF reader object.
-        pdf_reader = PyPDF2.PdfFileReader(file)
+def save_text_as_pdf(text, template_path, file_name):
+    # open the existing PDF template
+    with open(template_path, 'rb') as template_file:
+        # create a PyPDF2 PdfFileReader object from the template
+        template_pdf = PyPDF2.PdfReader(template_file)
 
-        # Create a PDF writer object.
-        pdf_writer = PyPDF2.PdfFileWriter()
+        # create a new PDF document
+        output_pdf = PyPDF2.PdfWriter()
 
-        # Get the number of pages in the PDF file.
-        num_pages = pdf_reader.getNumPages()
+        # loop through each page of the template and add it to the output PDF
+        for i in range(len(template_pdf.pages)):
+            page = template_pdf.pages[i]
+            output_pdf.add_page(page)
 
-        # Iterate through all the pages and add the text.
-        for page in range(num_pages):
-            # Get the current page object.
-            pdf_page = pdf_reader.getPage(page)
+        # create a new page and add the text to it
+        new_page = PageObject.create_blank_page(None, output_pdf.pages[0].mediabox.width, output_pdf.pages[0].mediabox.height)
+        new_page.merge_page(output_pdf.pages[0])
+        # content_stream = PyPDF2.pdf.ContentStream([PyPDF2.pdf.TextObject.createTextObject(output_pdf, text)], new_page)
+        # new_page._contentStreams = [content_stream]
+        output_pdf.add_page(new_page)
 
-            # Add the text to the page.
-            pdf_page.mergePage(PyPDF2.pdf.PageObject.createTextObject(text))
+        # save the output PDF to a file
+        with open(file_name, 'wb') as output_file:
+            output_pdf.write(output_file)
 
-            # Add the updated page to the writer.
-            pdf_writer.addPage(pdf_page)
 
-        # Save the updated PDF file.
-        with open(path, 'wb') as output:
-            pdf_writer.write(output)
+def redact_string(input_string, regex_pattern):
+    redacted_string = re.sub(regex_pattern, "[REDACTED]", input_string)
+    return redacted_string
 
+def main(input_path, output_path):
+    text = extract_text_from_pdf(input_path)
+    regex_arr = [
+        r"\d\d\d-\d\d-\d\d\d\d",  # Social Security 123-12-1234
+        r"\d\d/\d\d/\d\d\d\d",  # Birthdays 11/30/1980
+        r"^[A-Za-z]{2}\d{2}-\d{5}-\d{4}$",  # Driver License Numbers D123456789123
+        r"\d\d\d\d\d\d\d\d\d",  # Routing Number 123456789
+        r"\(\d\d\d\)\d\d\d-\d\d\d\d"  # Phone Numbers (231)667-503
+    ]
+
+    for regex in regex_arr:
+        text = redact_string(text, regex)
+
+    save_text_as_pdf(text, input_path, output_path)
     
-    
-
-def tokenize(text):
-    """Tokenizes a string based on words.
-
-    Args:
-        text (str): The input string to tokenize.
-
-    Returns:
-        list: A list of words in the input string.
-    """
-    tokens = text.split()
-    return tokens
 
 
 
 if __name__ == '__main__':
-    # main stuff goes here
-    text = extract_text_from_pdf('examples/test.pdf')
-    print(tokenize(text))
-    # insert_text_into_pdf('examples/test2.pdf', 'Hello World')
+   main("examples/SensitiveInfo.pdf", "examples/output.pdf")
