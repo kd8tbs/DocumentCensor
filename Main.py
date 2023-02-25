4c426ee30766cc56988d9f3428bfe692096cfcdd
Gary Pinsky <kd8tbs@gmail.com>
2023-02-24 21:14:35 -0500

    it broke

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
 import PyPDF2
 from PyPDF2 import PdfReader
+import os
+from io import BytesIO
+from PyPDF2 import PdfReader, PdfWriter
+import re
 
 def extract_text_from_pdf(path):
     """
@@ -11,38 +15,36 @@ def extract_text_from_pdf(path):
         text += page.extract_text() + "\n"
     return text
 
+def create_pdf_with_text(text, input_path, output_path):
+    # Open the original PDF file
+    with open(input_path, 'rb') as f:
+        pdf = PdfReader(f)
 
-def insert_text_into_pdf(path, text):
-    """
-    Inserts text into a PDF file and saves the updated file.
-    """
-    # Open the PDF file in read-binary mode.
-    with open(path, 'rb') as file:
-        # Create a PDF reader object.
-        pdf_reader = PyPDF2.PdfFileReader(file)
+        # Create a new PDF writer
+        writer = PdfWriter()
 
-        # Create a PDF writer object.
-        pdf_writer = PyPDF2.PdfFileWriter()
+        # Add the document information to the new PDF writer
+        writer.add_metadata(pdf.metadata)
 
-        # Get the number of pages in the PDF file.
-        num_pages = pdf_reader.getNumPages()
+        # Add each page from the original PDF to the new PDF writer
+        for page_num in range(len(pdf.pages)):
+            # page = pdf.getPage(page_num)
+            page = pdf.pages[page_num]
+            writer.add_page(page)
 
-        # Iterate through all the pages and add the text.
-        for page in range(num_pages):
-            # Get the current page object.
-            pdf_page = pdf_reader.getPage(page)
+        # Add a new page with the extracted text
+        new_page = writer.add_blank_page(width=200, height=200) # Change the width and height to suit your needs
+        new_page.merge_page(PdfReader(BytesIO(text.encode())).getPage(0))
+        new_page.mergeTranslatedPage(new_page, dx=0, dy=100, expand=False) # Change the dx and dy values to adjust the position of the added text
 
-            # Add the text to the page.
-            pdf_page.mergePage(PyPDF2.pdf.PageObject.createTextObject(text))
+        # Write the new PDF to a BytesIO buffer
+        output_buffer = BytesIO()
+        writer.write(output_buffer)
 
-            # Add the updated page to the writer.
-            pdf_writer.addPage(pdf_page)
+    # Write the BytesIO buffer to the output file
+    with open(output_path, 'wb') as f:
+        f.write(output_buffer.getvalue())
 
-        # Save the updated PDF file.
-        with open(path, 'wb') as output:
-            pdf_writer.write(output)
-
-    
     
 
 def tokenize(text):
@@ -58,8 +60,50 @@ def tokenize(text):
     return tokens
 
 
+def redact_strings(arr, regex_filter):
+    """ 
+    Redacts strings in an array that match a regex filter.
+
+    Parameters:
+    arr (list): An array of strings
+    regex_filter (str): A regex pattern to filter the strings
+
+    Returns:
+    list: A modified array where strings matching the regex filter are replaced with "[REDACTED]"
+    """
+    redacted_arr = []
+    for string in arr:
+        if re.search(regex_filter, string):
+            redacted_arr.append("[REDACTED]")
+        else:
+            redacted_arr.append(string)
+    return redacted_arr
 
 if __name__ == '__main__':
     # main stuff goes here
-    print(extract_text_from_pdf('examples/test.pdf'))
-    # insert_text_into_pdf('examples/test2.pdf', 'Hello World')
+    text = extract_text_from_pdf('examples/SensitiveInfo.pdf')
+    # create_pdf_with_text(extract_text_from_pdf('examples/test.pdf'), 'examples/test.pdf', 'examples/test2.pdf') # TODO: Fix this
+    regex_arr = [
+    r"^\d{3}-\d{2}-\d{4}$",  # Social Security
+    r"^\d{4}-\d{2}-\d{2}$",  # Birthdays
+    r"^\d+\s+\w+\s+\w+(\s+\w+)?,\s+\w+\s+\d{5}(-\d{4})?$",  # Addresses
+    r"^\(\d{3}\)\d{3}-\d{4}$",  # Phone Numbers
+    r"^[A-Za-z]{2}\d{2}-\d{5}-\d{4}$",  # Driver License Numbers
+    r"^\d{9}$"  # Routing Number
+    ]
+    tokenized_text = tokenize(text)
+    
+    for regex in regex_arr:
+        print(regex)
+        tokenized_text = redact_strings(tokenized_text, regex)
+    
+    print(tokenized_text)
+    arr = ["password123", "user123", "admin", "admin123", "(989)842-7702"]
+    regex_filter = r"^\(\d{3}\)\d{3}-\d{4}$"
+
+    redacted_arr = redact_strings(arr, regex_filter)
+
+    print(redacted_arr) # prints ["[REDACTED]", "user123", "[REDACTED]", "[REDACTED]"]
+
+
+    
